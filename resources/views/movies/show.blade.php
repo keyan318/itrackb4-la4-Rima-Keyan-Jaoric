27812269c1e9923de9a7d970a2a7ebbc0cc5f4cc
Keyan Jaoric Rima <keyanjaoricrima@Keyans-MacBook-Neo.local>
2026-09-12 20:31:03 +0800

    Updated the show.blade

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,87 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $item['title'] }}</title>
+@extends('layours.app')
 
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background: #f4f6f8;
-            margin: 0;
-            padding: 40px;
-        }
+@section('title', $item['title'])
 
-        .container {
-            max-width: 600px;
-            margin: auto;
-            background: white;
-            padding: 30px;
-            border-radius: 12px;
-            box-shadow: 0 4px 15px rgba(0, 0, 0, 0.08);
-        }
+@section('content')
 
-        h1 {
-            margin-top: 0;
-            margin-bottom: 25px;
-            color: #222;
-        }
+<div class=card"">
+    <div class="card-body">
+        <h2>{{$item['title']}}</h2>
 
-        .details {
-            border-top: 1px solid #ddd;
-            padding-top: 15px;
-        }
-
-        .details p {
-            margin: 15px 0;
-            color: #444;
-        }
-
-        .back {
-            display: inline-block;
-            margin-top: 15px;
-            padding: 10px 16px;
-            background: #222;
-            color: white;
-            text-decoration: none;
-            border-radius: 6px;
-        }
-
-        .back:hover {
-            background: #444;
-        }
-
-        footer {
-            text-align: center;
-            margin-top: 25px;
-            color: #888;
-            font-size: 14px;
-        }
-    </style>
-</head>
-
-<body>
-
-    <div class="container">
-
-        <h1>🎬 {{ $item['title'] }}</h1>
-
-        <div class="details">
-            <p><strong>Genre:</strong> {{ $item['genre'] }}</p>
-            <p><strong>Year:</strong> {{ $item['year'] }}</p>
-            <p><strong>Director:</strong> {{ $item['director'] }}</p>
-            <p><strong>Rating:</strong> {{ $item['rating'] }}</p>
-            <p><strong>Duration:</strong> {{ $item['duration'] }}</p>
-        </div>
-
-        <a class="back" href="{{ route('movies.index') }}">
-            ← Back to List
-        </a>
-
-        
+        <p><strong>Genre:</strong>{{$item['genre']}}</p>
+        <p><strong>Year:</strong>{{$item['year']}}</p>
+        <p><strong>Director:</strong>{{$item['director']}}</p>
+        <p><strong>Rating:</strong>{{$item['rating']}}</p>
+        <p><strong>Duration</strong>{{$item['duration']}}</p>
     </div>
-
-</body>
-</html>
-
+    <a  class="btn btn-primary"  href="{{route('movies.index')}}"></a>
+</div>
