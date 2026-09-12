82b02eb69b82348070eb4317df74c154ac5dc4eb
Keyan Jaoric Rima <keyanjaoricrima@Keyans-MacBook-Neo.local>
2026-09-12 19:40:52 +0800

    Created a partials/_nav.blade

---
+++ resources/views/partials/_nav.blade.php
@@ -0,0 +1,6 @@
+<nav class="nav">
+    <a class="nav-link" href="{{route('movies.index')}}">All Movies</a>
+    <a class="nav-link" href="{{route('movies.filtered')}}">Action Movies</a>
+    
+
+</nav>
